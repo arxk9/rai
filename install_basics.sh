fbf5bc0cd6821d3ff973c7dabf7f946f2b5e15e4
rsl <junja94@gmail.com>
2017-09-07 12:39:35 +0200

    lib cairo install added

--- install_basics.sh
+++ install_basics.sh
@@ -130,12 +130,15 @@ if [ "$yrelease" -eq "16" ]; then
 else 
     if [ "$yrelease" -eq "14" ]; then
      cd $RAI_ROOT/dependencies
-     sudo apt-get install libqt4-dev
+     # plotting dependencies
+     sudo apt-get install $APT_GET_FLAGS libqt4-dev
+     sudo apt-get install $APT_GET_FLAGS libcairo2-dev
+     sudo apt-get install $APT_GET_FLAGS libpango1.0-dev
      wget https://downloads.sourceforge.net/project/gnuplot/gnuplot/5.0.5/gnuplot-5.0.5.tar.gz
      tar -xvzf gnuplot-5.0.5.tar.gz
      rm gnuplot-5.0.5.tar.gz
      cd gnuplot-5.0.5/
-     ./configure --prefix=/usr --disable-wxwidgets --with-pdf --with-qt  && sudo make all -j && sudo make install
+     ./configure --prefix=/usr --disable-wxwidgets && sudo make all -j && sudo make install
      cd $RAI_ROOT
     fi
 fi
