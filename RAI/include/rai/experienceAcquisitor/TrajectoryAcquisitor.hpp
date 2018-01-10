f27aaa8bf2167bd8c56448050c7366f18caea43e
joonho <junja94@gmail.com>
2018-01-10 23:41:09 +0100

    make it readable

--- RAI/include/rai/experienceAcquisitor/TrajectoryAcquisitor.hpp
+++ RAI/include/rai/experienceAcquisitor/TrajectoryAcquisitor.hpp
@@ -144,14 +144,14 @@ class TrajectoryAcquisitor : public Acquisitor<Dtype, StateDim, ActionDim> {
     StateBatch VineStartPosition(StateDim, numofjunct);
     StateBatch rolloutstartState(StateDim, numofjunct * numOfBranchPerJunct);
     trajectories.resize(numofjunct * numOfBranchPerJunct);
-    rolloutstartState.setOnes();
     std::vector<std::pair<int, int> > indx;
     rai::Op::VectorHelper::sampleRandomStates(traj, VineStartPosition, int(0.1 * timeLimit / dt), indx);
 
-    for (int dataID = 0; dataID < numofjunct; dataID++)
-      rolloutstartState.block(0, dataID * numOfBranchPerJunct, StateDim, numOfBranchPerJunct) =
-          rolloutstartState.block(0, dataID * numOfBranchPerJunct, StateDim, numOfBranchPerJunct).array().colwise()
-              * VineStartPosition.col(dataID).array();
+    for (int junctID = 0; junctID < numofjunct; junctID++)
+      for (int branchID = 0; branchID < numOfBranchPerJunct; branchID++ )
+      rolloutstartState.col(junctID * numOfBranchPerJunct + numOfBranchPerJunct) =
+          VineStartPosition.col(junctID).array();
+
 
     for (auto &tra : trajectories)
       tra.clear();
